a62d4682d2b7cb5c38a356e7456d0f4f0be435a4
John Edward Oblepias <johnedwardoblepias11@gmail.com>
2025-09-13 13:39:20 +0800

    Refactor ConstructionPermit model: update fillable and casts properties for better data handling

--- app/Models/ConstructionPermit.php
+++ app/Models/ConstructionPermit.php
@@ -9,12 +9,16 @@ class ConstructionPermit extends Model
     protected $table = 'construction_permit';
     protected $primaryKey = 'permit_no';
     public $timestamps = false;
+    const CREATED_AT = 'timestamp';
+    const UPDATED_AT = null;
 
     protected $fillable = [
         'mem_id',
         'application_date',
         'applicant_name',
         'applicant_contact',
+        'contractor_name',
+        'contractor_contact',
         'permit_type',
         'permit_sin',
         'permit_arn',
@@ -32,19 +36,28 @@ class ConstructionPermit extends Model
         'bond_receiver',
         'bond_release_date',
         'remarks',
-        'user_id'
+        'user_id',
+        'timestamp'
     ];
 
     protected $casts = [
+        'permit_no' => 'integer',
+        'mem_id' => 'integer',
         'application_date' => 'date',
+        'permit_type' => 'integer',
+        'permit_sin' => 'integer',
+        'permit_arn' => 'integer',
+        'permit_fee' => 'decimal:2',
+        'permit_bond' => 'decimal:2',
         'permit_fee_date' => 'date',
         'permit_bond_date' => 'date',
         'permit_start_date' => 'date',
         'permit_end_date' => 'date',
+        'status_type' => 'integer',
         'inspection_date' => 'date',
         'bond_release_date' => 'date',
-        'permit_fee' => 'decimal:2',
-        'permit_bond' => 'decimal:2'
+        'user_id' => 'integer',
+        'timestamp' => 'datetime'
     ];
 
     // Relationship with MemberData
